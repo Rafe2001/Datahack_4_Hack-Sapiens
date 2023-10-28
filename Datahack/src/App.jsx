3ca37303349d5ae8af809f718b061a79fd772c32
allan <kingallan.10@gmail.com>
2023-10-28 19:17:05 +0530

    first commit

--- Datahack/src/App.jsx
+++ Datahack/src/App.jsx
@@ -1,41 +1,32 @@
-import './App.css'
+import './App.css';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
-import { styled } from '@mui/material/styles';
-import { SidebarWithSearch } from './components/Sidebar'
+import { SidebarWithSearch } from './components/Sidebar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
 
 function App() {
-
   return (
-    <>
-
-      <div className='text-3xl font-bold'>
-      <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2}>
-        <Grid item xs={4} className='border-1px-solid'>
-        <SidebarWithSearch />
-
-        </Grid>
-        <Grid item xs={8}  className='border-10px-solid'>
-          hi
-          ernvewjbvj
-        </Grid>
-          {/* <Grid item xs={4}>
-            hi
-          </Grid>
-          <Grid item xs={8}>
-            hi
-          </Grid> */}
-      </Grid>
-    </Box>
-       
-        
-      </div>
-
-
-
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={
+          <div className='text-3xl font-bold'>
+            <Box sx={{ flexGrow: 1 }}>
+              <Grid container spacing={2}>
+                <Grid item xs={4} className='border-1px-solid'>
+                  <SidebarWithSearch />
+                </Grid>
+                <Grid item xs={8} className='border-10px-solid'>
+                  hi ernvewjbvj
+                </Grid>
+              </Grid>
+            </Box>
+          </div>
+        } />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
